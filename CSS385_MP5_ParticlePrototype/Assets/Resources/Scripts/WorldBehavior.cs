096c397b32209c4094a69107e8e6b433ee16037d
stanh2 <stan.huber.c@gmail.com>
2017-05-02 08:18:14 -0700

    Particle Systems Prototype
    This commit contains the Particle Systems Prototype for the Shred
    prototypes project. This is the first commit and is temporarily the
    master. Eventually, the master will contain a skeleton for the main menu
    UI (not functioning) with buttons to load each of the five prototypes as
    a scene for demonstration.  Ideally the master would contain the main
    menu UI and each developer could branch the master to add in their
    prototype and have it pulled to the master.

---
+++ CSS385_MP5_ParticlePrototype/Assets/Resources/Scripts/WorldBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBehavior : MonoBehaviour {
+    GameObject hero = null;
+    public Transform snowBurst;
+
+    // Use this for initialization
+    void Start() {
+        //hero = (GameObject)Instantiate(Resources.Load("Prefabs/Penguin"));
+        snowBurst.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        snowBurst.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(timeSnowBurst());
+    }
+
+    
+    IEnumerator timeSnowBurst()
+    {
+        yield return new WaitForSeconds(0.8f);
+        snowBurst.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
