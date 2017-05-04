1aab8069b02b98096555b7c1b7b47b1f610a81f6
ShaneleeTran <ccforgeking@gmail.com>
2017-05-03 19:04:53 -0700

    Updated Player hit boxes

--- CSS385_MP5_ParticlePrototype/Assets/LandingAndTricksResources/Scripts/playerBehavior.cs
+++ CSS385_MP5_ParticlePrototype/Assets/LandingAndTricksResources/Scripts/playerBehavior.cs
@@ -10,10 +10,12 @@ public class playerBehavior : MonoBehaviour {
     public const float maxSpeed = 50f;    
     public const float jumpHeight = 100f;
     public const float rotationSpeed = 200f;
+    public const float grindSpeed = 50;
     public float speedBoostTime = 2f; // the duration the speed boost is applied for
     private float speedMultiplier;
     private bool jumped = true;
     private bool boost = false;
+    private bool isAboveRail = false;
     private float initRotation;
     private GameObject gm;
     private GlobalBehavior gb;
@@ -24,6 +26,11 @@ public class playerBehavior : MonoBehaviour {
     private bool isOnGround = false;
     private Rigidbody2D mRB;
     private float groundCheckerRadius = 0.5f;
+
+    // head checker variables
+    public Transform headChecker;
+    private float headCheckerRadius = 0.8f;
+
     #endregion
 
     // Use this for initialization
@@ -51,7 +58,7 @@ public class playerBehavior : MonoBehaviour {
             isOnGround = false;
             jumped = true;
             mRB.AddForce(new Vector2(0, jumpHeight));
-            gm.GetComponent<GlobalBehavior>().UpdateLandingText("Landing: In Air");
+            gm.GetComponent<GlobalBehavior>().UpdateLandingText("Landing: In Air");            
         }
     }
 
@@ -60,6 +67,10 @@ public class playerBehavior : MonoBehaviour {
         // checks if the player is contacting the ground
         isOnGround = Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, groundLayer);
 
+        // checks if player's head has hit the ground
+        if (Physics2D.OverlapCircle(headChecker.position, headCheckerRadius, groundLayer))
+            gb.DestroyMe();
+
         // prevents character from move faster than the max speed;
         if(mRB.velocity.magnitude >= maxSpeed)
         {
@@ -67,14 +78,20 @@ public class playerBehavior : MonoBehaviour {
         }
 
         // player can only rotate when in the air
-        if (!isOnGround)
+        if (!isOnGround && !isAboveRail)
         {
             mRB.MoveRotation(mRB.rotation - Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime);
             // mRB.AddTorque(Input.GetAxis("Horizontal") * -rotationSpeed);  different rotation method feels different
         }
 
+        if (isAboveRail && Input.GetAxis("Vertical") < 0)
+        {
+            mRB.AddForce(transform.right * grindSpeed);
+            //mRB.position.y = ;
+        }        
+
         // apply speed boost for x amount of seconds
-        if(boost)
+        if (boost)
         {
             speedBoostTime -= Time.deltaTime;
             if(speedBoostTime > 0)
@@ -100,7 +117,8 @@ public class playerBehavior : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("GroundCollider") && jumped)
+        #region ground trigger
+        if (other.gameObject.CompareTag("GroundCollider") && jumped)
         {
             float angle = Vector2.Angle(this.transform.right, other.transform.right);
             //Debug.Log(angle);
@@ -157,9 +175,19 @@ public class playerBehavior : MonoBehaviour {
                 boost = true;
                 jumped = false;
                 gb.UpdateLandingText("Landing: CRASH! +" + reward.ToString());
-                gb.DestryMe();
+                gb.DestroyMe();
             }           
-        }        
+        }
+        #endregion
+
+        #region rail trigger
+        if (other.gameObject.CompareTag("GrindObject"))
+        {
+            isAboveRail = true;
+        }
+        else
+            isAboveRail = false;
+        #endregion
     }
 
     void ApplySpeedBoost()
@@ -194,8 +222,7 @@ public class playerBehavior : MonoBehaviour {
                 gb.UpdateTrickText("Trick: Backflip +" + reward.ToString());
             }
                 
-        }            
-
+        }
         //Debug.Log(deltaAngle);
     }
 }
